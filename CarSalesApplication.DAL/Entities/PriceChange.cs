6e08da9d9dbaee7b140d4a8ba8ac0dd90943efec
Turgay Ceylan <90829101+turgay2317@users.noreply.github.com>
2025-02-20 22:23:58 +0300

    Price-Change-Entities-Are-Removed

--- CarSalesApplication.DAL/Entities/PriceChange.cs
+++
@@ -1,9 +0,0 @@
-namespace CarSalesApplication.DAL.Entities;
-
-public class PriceChange
-{
-    public int CarId { get; set; }
-    public decimal OldPrice { get; set; }
-    public decimal NewPrice { get; set; }
-    public DateTime ChangeDate { get; set; }
-}
